f6121478b89e3895dbc0bd5471154ef32c3572a2
Asheeti Patel <asheetirpatel@gmail.com>
2026-05-08 21:40:55 +0530

    Delete 50 sql bdge Q directory

--- 50 sql bdge Q/Delete Duplicate emails.sql
+++
@@ -1,6 +0,0 @@
--- Delete duplicate emails keeping the record with the smallest id
-DELETE p 
-FROM Person p
-JOIN Person p2 
-  ON p.email = p2.email 
-  AND p.id > p2.id;
--- 50 sql bdge Q/Find users with valid emails.sql
+++
@@ -1,4 +0,0 @@
--- Select valid emails from the Users table
-SELECT *
-FROM Users
-WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$';
